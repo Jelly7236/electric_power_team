f38c7f669711cd00875f4eb4d562c0cfd53dccbb
Jelly7236 <eeak8181@naver.com>
2025-11-04 22:37:59 +0900

    워드 경로 수정

--- 대시보드/pages/second.py
+++ 대시보드/pages/second.py
@@ -277,7 +277,8 @@ CHART_COLORS = {
 TRAIN_PATH = "대시보드/data_dash/train_dash_df.csv"
 MONTHLY_PF_PATH =  "대시보드/data_dash/월별 역률 패널티 계산.csv"
 RATE_PDF = Path("대시보드/data_dash/2024년도7월1일시행전기요금표(종합)_출력용.pdf")
-TEMPLATE_PATH = Path("대시보드/data_dash/고지서_템플릿.docx")
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATE_PATH = BASE_DIR / "data_dash" / "고지서_템플릿.docx"
 
 # ============================================================================
 # 유틸리티 함수
